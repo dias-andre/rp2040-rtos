dffbf5d2984d1b9f9eca456a99294a535195f93c
https-dre <diaso.andre@outlook.com>
2026-02-25 16:45:25 -0300

    chore(_start): remove old comments

--- src/main.zig
+++ src/main.zig
@@ -3,39 +3,17 @@ const gpio = @import("hal/gpio.zig");
 const clocks = @import("hal/clocks.zig");
 const timer = @import("hal/timer.zig");
 
-// Entrypoint
 export fn _start() linksection(".text.entry") noreturn {
-
-    // 1. Take the pin controller (IO_BANK0) out of the Reset state (controlled by bit 5)
-    // RESETS_RESET.* &= ~@as(u32, 1 << 5);
-
-    // Wait for the hardware to confirm it has powered up
-    // while ((RESETS_DONE.* & (1 << 5)) == 0) {}
     gpio.enable_io_bank();
     clocks.crystal_init();
     timer.enable_timer();
 
-    // 2. Configure pin 25 to be controlled via software (SIO = function 5)
-    // IO_BANK0_GPIO25_CTRL.* = 5;
     gpio.init(25);
 
-    // 3. Set pin 25 as OUTPUT (Output Enable)
-    // SIO_GPIO_OE.* |= (1 << 25);
     gpio.set_output_mode(25);
 
-    // 4. The Kernel's Infinite Loop
     while (true) {
-        // Invert the current state of pin 25 (mathematical XOR)
-        // SIO_GPIO_OUT_XOR.* = (1 << 25);
         gpio.toggle_pin(25);
-
-        // A "raw" delay burning CPU cycles
-        // var i: u32 = 0;
-        // while (i < 500000) {
-        //     // Tell the compiler not to optimize away this loop
-        //     asm volatile ("nop");
-        //     i += 1;
-        // }
         timer.sleep(5000);
     }
 }
