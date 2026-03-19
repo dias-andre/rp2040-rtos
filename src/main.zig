35a4e672c70a41d5f3f26a80af5f16547b18e660
https-dre <diaso.andre@outlook.com>
2026-03-19 19:40:14 -0300

    refactor(main): update ImageDefBlock structure and simplify GPIO handling in _start function

--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,10 @@ const vectors = @import("vectors.zig");
 pub const ImageDefBlock = extern struct {
     magic_start: u32 = 0xFFFFEADB,
     image_type: u32 = 0x00000102,
-    next_block: u32 = 0x00000000,
-    last_item: u32 = 0x00000200,
+    image_info: u32 = 0x00000001,
+    vt_type: u32 = 0x00000103,
+    vt_offset: u32 = 0x00000100,
+    end_tag: u32 = 0x00000000,
     magic_end: u32 = 0xAB123579,
 };
 
@@ -26,23 +28,16 @@ const timer = @import("hal/timer.zig");
 export fn _start() noreturn {
     gpio.init();
     clocks.crystal_init();
-    clocks.enable_pll();
+    // clocks.enable_pll();
     timer.enable_timer();
 
     gpio.init_pin(25);
-    gpio.init_pin(24);
 
     gpio.set_output_mode(25);
-    gpio.set_pullup(24);
     gpio.write_low(25);
 
-    gpio.write_high(25);
-    timer.sleep(500);
-    gpio.write_low(25);
     while (true) {
-        if (gpio.read_pin(24) == false) {
-            gpio.toggle_pin(25);
-        }
+        gpio.toggle_pin(25);
         timer.sleep(100);
     }
 }
